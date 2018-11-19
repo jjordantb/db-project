8bd847cb282b06f6a8165f706bb6ee9b22073ea7
jjordantb <jordan.florchinger@uleth.ca>
2018-11-19 10:03:26 -0700

    adwjiopawd

--- src/Test.py
+++ src/Test.py
@@ -1,52 +1,53 @@
-# import ImgUtil
-# import numpy as np
-# import scipy.misc as smp
-#
-# from YCluster import YCluster
-#
-# imgs0 = ImgUtil.load_mnist(0)
-# cluster = YCluster(imgs0[0])
-# cluster.add_vector(imgs0[1])
-# cov = np.cov([imgs0[0], imgs0[1]], rowvar=False)
-# print(np.array_equal(cluster.cov_matrix, cov))
+import ImgUtil
+import numpy as np
+import scipy.misc as smp
+
+from YCluster import YCluster
+
+imgs0 = ImgUtil.load_mnist(0)
+cluster = YCluster(imgs0[0])
+cluster.add_vector(imgs0[1])
+cov = np.cov([imgs0[0], imgs0[1]], rowvar=False)
+print(cov)
+print(cluster.cov_matrix)
 
 # print(vals)
 # print(cov)
 # cov2 = np.cov([np.cov([imgs0[0], imgs0[1]], rowvar=False), imgs0[2]], rowvar=False)
 # print(cov2)
-import sys
-
-from Node import Node
-import ImgUtil
-import numpy as np
-
-data = []  # tuple of (image data vector, 1-hot-vector)
-for i in range(0, 10):
-    imgs = ImgUtil.load_mnist(i)
-    vect = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    vect[i] = 1
-    for img in imgs:
-        data.append((img, vect))
-
-
-def test(node, x_in):
-    closest = None
-    distance = sys.maxsize
-    index = 0
-    i = 0
-    for x_cluster in node.x_clusters:
-        tmp_distance = np.linalg.norm(x_cluster.cov_matrix - np.cov(x_in, rowvar=False))
-        if tmp_distance < distance:
-            closest = x_cluster
-            distance = tmp_distance
-            index = i
-        i += 1
-    print(index)
-    return node.y_clusters[index].mean_vector
-
-
-node = Node(10)
-node.build_tree(data, 1)
-print(len(node.x_clusters))
-print(test(node, data[0][0]))
+# import sys
+#
+# from Node import Node
+# import ImgUtil
+# import numpy as np
+#
+# data = []  # tuple of (image data vector, 1-hot-vector)
+# for i in range(0, 10):
+#     imgs = ImgUtil.load_mnist(i)
+#     vect = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#     vect[i] = 1
+#     for img in imgs:
+#         data.append((img, vect))
+#
+#
+# def test(node, x_in):
+#     closest = None
+#     distance = sys.maxsize
+#     index = 0
+#     i = 0
+#     for x_cluster in node.x_clusters:
+#         tmp_distance = np.linalg.norm(x_cluster.cov_matrix - np.cov(x_in, rowvar=False))
+#         if tmp_distance < distance:
+#             closest = x_cluster
+#             distance = tmp_distance
+#             index = i
+#         i += 1
+#     print(index)
+#     return node.y_clusters[index].mean_vector
+#
+#
+# node = Node(10)
+# node.build_tree(data, 1)
+# print(len(node.x_clusters))
+# print(test(node, data[0][0]))
 
--- src/YCluster.py
+++ src/YCluster.py
@@ -5,17 +5,16 @@ class YCluster:
 
     def __init__(self, first_vector):
         self.mean_vector = first_vector
-        self.cov_matrix = first_vector
+        self.cov_matrix = np.cov([first_vector], rowvar=False)
         self.vectors = [first_vector]
         self.size = 1
 
     def add_vector(self, new_vect):
         old_vector = np.array(self.mean_vector) * self.size
         self.mean_vector = (np.array(old_vector) + np.array(new_vect)) / (self.size + 1)
-        # variance = np.cov(new_vect, rowvar=False)
-        # self.cov_matrix = (variance + self.mean_vector) / (self.size + 1)
+        variance = np.cov(new_vect, rowvar=False)
+        self.cov_matrix = (variance + self.mean_vector) / (self.size + 1)
         self.vectors.append(new_vect)
-        self.cov_matrix = np.cov(self.vectors, rowvar=False)
         self.size += 1
 
     def contains_vector(self, vector):
